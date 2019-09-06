9d0686803331fcbd1f0e78782eb4ad8a1daf4037
Thomas Coopman <thomas.coopman@gmail.com>
2019-09-06 10:07:38 +0200

    some extra queries

--- queries.sh
+++ queries.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 echo "Total"; cat $1 | jq 'length'
+echo "GameWasStarted"; cat $1 | jq '[.[] | select(.type == "GameWasStarted")] | length'
+echo "GameWasFinished"; cat $1 | jq '[.[] | select(.type == "GameWasFinished")] | length'
+echo "GameWasCancelled"; cat $1 | jq '[.[] | select(.type == "GameWasCancelled")] | length'
 echo "PlayerHasRegisterd"; cat $1 | jq '[.[] | select(.type == "PlayerHasRegistered") | .payload.owner_id] | length'
 echo "QuizWasCreated"; cat $1 | jq '[.[] | select(.type == "QuizWasCreated") | .payload.owner_id] | sort | length'
 echo "GameWasOpened"; cat $1 | jq '[.[] | select(.type == "GameWasOpened")] | length'

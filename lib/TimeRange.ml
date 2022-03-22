8c9c40e7fcf62c2fc5a55c1e81c3b245db6cbd71
Thomas Coopman <thomas.coopman@gmail.com>
2022-03-22 11:12:15 +0100

    end in 2022

--- lib/TimeRange.ml
+++ lib/TimeRange.ml
@@ -3,7 +3,7 @@ type t = { startTimestamp : float; endTimestamp : float }
 let daysUntilNow ~days =
   let open CalendarLib.Calendar in
   let period = CalendarLib.Calendar.Precise.Period.day days in
-  let now = Precise.make 2019 09 20 10 00 0 in
+  let now = Precise.make 2022 03 22 10 00 0 in
   let start = Precise.add now (Precise.Period.opp period) in
   {
     startTimestamp = Precise.to_unixfloat start;
